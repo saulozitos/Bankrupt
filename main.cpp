99ffd99dbbfee34e0856821a98bf748f2be53166
Saulo Campos <contatosauloaraujo@gmail.com>
2020-12-20 13:24:01 -0300

    refatoração

--- main.cpp
+++ main.cpp
@@ -14,31 +14,29 @@ int main()
 {
     const auto *const fileConfiguration = "/home/saulo/Dev/Bankrupt/gameConfig.txt";
     auto game = std::make_unique<Game>(fileConfiguration);
-    std::vector<Player*>players;
 
     // ​Compra qualquer propriedade sobre a qual ele parar.
     auto player1 = std::make_unique<Player>("Player1");
-    players.push_back(player1.get());
 
     // ​Compra qualquer propriedade, desde que o aluguel dela seja maior do​ ​que 50 ​coins.
     auto player2 = std::make_unique<Player>("Player2");
-    players.push_back(player2.get());
 
     // ​Compra qualquer propriedade, desde que ao final da compra ele possua uma reserva maior ou igual a​ ​80 ​coins​.
     auto player3 = std::make_unique<Player>("Player3");
-    players.push_back(player3.get());
 
     // Compra a propriedade de maneira aleatória, com probabilidade de​ ​50%.
     auto player4 = std::make_unique<Player>("Player4");
-    players.push_back(player4.get());
+
+    std::vector<Player*>players {player1.get(), player2.get(), player3.get(), player4.get()};
 
     game->setTotalPlayers(players.size());
 
     unsigned short totalMatch{300};
     unsigned short round{0};
+    unsigned short timeOutRound{1000};
     unsigned short totalRounds{0};
 
-    // player name, timeout wins, game over wins
+    // player name, timeout wins, normal wins
     std::map<std::string, std::pair<int, int>>countWins{
         {player1->getId(), {0,0}},
         {player2->getId(), {0,0}},
@@ -48,9 +46,7 @@ int main()
 
     for(unsigned short match = 1; match <= totalMatch; ++match)
     {
-        std::cout << "Running simulation #" << match << std::endl;
-
-        while(!game->isGameOver() && round < 1000)
+        while(!game->isGameOver() && round < timeOutRound)
         {
             for(const auto &it : players)
             {
@@ -66,10 +62,9 @@ int main()
             round++;
         }
 
-        std::cout << "The game ended in round " << round << std::endl;
         totalRounds += round;
 
-        const bool isGameTimeout = round == 1000;
+        const bool isGameTimeout = round == timeOutRound;
         if(isGameTimeout || game->isGameOver())
         {
             std::vector<std::pair<int, std::string>>players{
@@ -81,12 +76,17 @@ int main()
 
             const auto winner = *std::max_element(players.begin(), players.end());
             const auto playerName = winner.second;
-            std::cout << "The winner is " << playerName << "\n" << std::endl;
 
             if(isGameTimeout)
+            {
+                std::cout<< "In simulation #" << match << " the game ended by time out - The winner is " << playerName << std::endl;
                 countWins[playerName].first += 1;
+            }
             else
+            {
+                std::cout<< "In simulation #" << match << " the game ended in round " << round << " - The winner is " << playerName << std::endl;
                 countWins[playerName].second += 1;
+            }
         }
 
         game.release();
@@ -95,21 +95,16 @@ int main()
         player3.release();
         player4.release();
         players.erase(players.begin(), players.end());
+
         round = 0;
 
         game = std::make_unique<Game>(fileConfiguration);
 
         player1 = std::make_unique<Player>("Player1");
-        players.push_back(player1.get());
-
         player2 = std::make_unique<Player>("Player2");
-        players.push_back(player2.get());
-
         player3 = std::make_unique<Player>("Player3");
-        players.push_back(player3.get());
-
         player4 = std::make_unique<Player>("Player4");
-        players.push_back(player4.get());
+        players = {player1.get(), player2.get(), player3.get(), player4.get()};
 
         game->setTotalPlayers(players.size());
     }
@@ -117,6 +112,7 @@ int main()
     int totalTimeOutWins{0};
     int totalNormalWins{0};
 
+    std::cout << "\nGamer analysis:" << std::endl;
     for(const auto &it : countWins)
     {
         const auto playerName = it.first;
@@ -125,7 +121,7 @@ int main()
         const auto normalWins = it.second.second;
         totalNormalWins += normalWins;
         const auto totalWins = timeOutWins + normalWins;
-        const float percentWiner = (totalWins * 100)/300.00;
+        const float percentWiner = (totalWins * 100.00)/totalMatch;
 
         std::cout << playerName
                   << " - time out wins: " << timeOutWins
