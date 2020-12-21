f503a5932673801599f798c5b954ae95e6c6348c
Saulo Campos <contatosauloaraujo@gmail.com>
2020-12-21 14:16:24 -0300

    refatoração

---
+++ simulation.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string_view>
+#include <memory>
+#include "game.h"
+class Game;
+class Simulation
+{
+public:
+    explicit Simulation(const std::string_view & file);
+    ~Simulation() = default;
+    void runSimulation();
+private:
+    std::string fileConfiguration;
+    std::unique_ptr<Game>game;
+};
+
